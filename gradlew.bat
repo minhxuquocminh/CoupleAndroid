ad2579f9a4be4df3806c4d78a2c2aefffa7ef9a5
minhxuquocminh <stylesofminh99@gmail.com>
2025-03-16 01:44:08 +0700

    revert file gradlew bat

--- gradlew.bat
+++ gradlew.bat
@@ -24,42 +24,42 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-numberSet DIRNAME=%~dp0
-if "%DIRNAME%" == "" numberSet DIRNAME=.
-numberSet APP_BASE_NAME=%~n0
-numberSet APP_HOME=%DIRNAME%
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do numberSet APP_HOME=%%~fi
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-numberSet DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-numberSet JAVA_EXE=java.exe
+set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is not numberSet and no 'java' command could be found in your PATH.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please numberSet the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
 
 :findJavaFromJavaHome
-numberSet JAVA_HOME=%JAVA_HOME:"=%
-numberSet JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is numberSet to an invalid directory: %JAVA_HOME%
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
-echo Please numberSet the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
